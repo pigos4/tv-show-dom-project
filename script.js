42a6b5b9cd98b03b1184a70bdfe9ef3966c359fc
Daniel <pigos487@gmail.com>
2022-02-20 13:00:51 +0000

    added all movied

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+const allEpisodes = getAllEpisodes();
+const rootElem = document.getElementById("root");
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+function render(arrOfIndexToRender) {
+  arrOfIndexToRender.forEach((e) => {
+    
+    const divContainer = document.createElement("div");
+    const pName = document.createElement("p");
+    const pSummary = document.createElement("p");
+    const img = document.createElement("img");
+divContainer.classList.add('singleMovie');
+    const singleObj = allEpisodes[e];
 
-window.onload = setup;
+    pName.innerText =
+      singleObj.name +
+      " S" +
+      ("0" + singleObj.season).slice(-2) +
+      "E" +
+      ("0" + singleObj.number).slice(-2);
+    img.src = singleObj.image.medium;
+    pSummary.innerHTML = singleObj.summary;
+rootElem.appendChild(divContainer);
+    divContainer.appendChild(pName);
+    divContainer.appendChild(img);
+    divContainer.appendChild(pSummary);
+  });
+}
+const clearChildren = () => {
+  while (rootElem.firstChild) {
+    rootElem.removeChild(rootElem.lastChild);
+  }
+};
+render(allEpisodes.map((v, i) => i));
+render([1, 2, 3]);
