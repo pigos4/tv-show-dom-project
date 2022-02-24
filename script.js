add18ee011a4e2b93f5f6b3ca331993de270e1e4
Daniel <pigos487@gmail.com>
2022-02-24 13:59:43 +0000

    level 300

--- script.js
+++ script.js
@@ -1,31 +1,121 @@
-const allEpisodes = getAllEpisodes();
-const rootElem = document.getElementById("root");
-
+// Utils
 const formatNumbers = (num) => (num < 10 ? `0${num}` : num.toString());
-const formatTitle = ({ name, season, number }) =>
-  `${name} S${formatNumbers(season)}E${formatNumbers(number)}`;
 
-function render(arrOfIndexToRender) {
-  arrOfIndexToRender.forEach((e) => {
+const formatTitle = ({ name, season, number }, seasonFirst) =>
+  !seasonFirst
+    ? `${name} S${formatNumbers(season)}E${formatNumbers(number)}`
+    : `S${formatNumbers(season)}E${formatNumbers(number)} - ${name} `;
+
+const clearChildren = (node, onlyOne) => {
+  if (onlyOne) {
+    node.removeChild(node.lastChild);
+  } else {
+    while (node.firstChild) {
+      node.removeChild(node.lastChild);
+    }
+  }
+};
+
+const movieCataloguePublisher = MovieCataloguePublisher();
+
+const movieCatalogueData = {
+  mc: [],
+  set(mc) {
+    console.log(mc);
+    this.movieCatalogue = mc;
+    searchBar.value = "";
+    selectMoviesElement.value = "";
+    movieCataloguePublisher.inputChange();
+  },
+  get() {
+    return this.movieCatalogue;
+  },
+};
+
+function MovieCataloguePublisher() {
+  let subscribers = [];
+
+  function subscribe(fn) {
+    subscribers.push(fn);
+  }
+  function unsubscribe(fn) {
+    subscribers = subscribers.filter((sub) => sub !== fn);
+  }
+  function inputChange(input) {
+    let filteredEpisodes = movieCatalogueData.get();
+    if (input) {
+      filteredEpisodes = movieCatalogueData
+        .get()
+        .filter(({ name, summary, season, number }) => {
+          return (
+            formatTitle({ name, season, number })
+              .toLowerCase()
+              .includes(input.toLowerCase()) ||
+            summary.toLowerCase().includes(input.toLowerCase())
+          );
+        });
+    }
+    subscribers.forEach((fn) => {
+      fn(filteredEpisodes);
+    });
+  }
+  return { subscribe, inputChange, unsubscribe };
+}
+
+const movieCatalogue = document.getElementById("movieCatalogue");
+// Creating select movies
+const selectMoviesElement = document.getElementById("selectMovies");
+
+function selectMovies() {
+  movieCatalogueData.get().forEach(({ name, season, number }, i) => {
+    const option = document.createElement("option");
+    option.text = `${formatTitle({ name, season, number }, true)}`;
+    option.value = name;
+    selectMoviesElement.add(option);
+  });
+}
+
+function clearSelect() {
+  selectMoviesElement.value = "";
+}
+selectMoviesElement.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  movieCataloguePublisher.inputChange(e.target.value);
+});
+
+//Creating config search bar
+const searchBar = document.getElementById("searchBar");
+searchBar.addEventListener("input", (e) => {
+  clearSelect();
+  movieCataloguePublisher.inputChange(e.target.value);
+});
+
+const amountResult = document.getElementById("amountResult");
+
+const renderMovies = (arrOfObjects) => {
+  clearChildren(movieCatalogue);
+  return arrOfObjects.map((e) => {
     const divContainer = document.createElement("div");
     const pName = document.createElement("p");
     const pSummary = document.createElement("p");
     const img = document.createElement("img");
     divContainer.classList.add("singleMovie");
-    const { image, summary } = (movie = allEpisodes[e]);
-
+    const { image, summary } = (movie = e);
     pName.innerText = formatTitle(movie);
     img.src = image.medium;
-    pSummary.innerHTML = summary;
-    rootElem.appendChild(divContainer);
+    pSummary.innerText = summary.replace(/<p>|<\/p>/g, "");
     divContainer.append(pName, img, pSummary);
+    movieCatalogue.appendChild(divContainer);
   });
-}
-const clearChildren = () => {
-  while (rootElem.firstChild) {
-    rootElem.removeChild(rootElem.lastChild);
-  }
 };
-render(allEpisodes.map((v, i) => i));
-
-render([1, 2, 3]);
+movieCataloguePublisher.subscribe(renderMovies);
+function renderAmountMovies(arrOfIndexToRender) {
+  amountResult.innerText = `(${arrOfIndexToRender.length}/${
+    movieCatalogueData.get().length
+  })`;
+}
+movieCataloguePublisher.subscribe(renderAmountMovies);
+window.onload = function () {
+  movieCatalogueData.set(getAllEpisodes());
+  selectMovies();
+};
