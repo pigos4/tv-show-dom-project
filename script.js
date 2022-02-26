75380d85ff6266caa145f8ebf4defc8a581a9160
Daniel <pigos487@gmail.com>
2022-02-26 20:19:48 +0000

    400

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const searchBar = document.getElementById("searchBar");
+const movieCatalogue = document.getElementById("movieCatalogue");
+const selectMoviesElement = document.getElementById("selectMovies");
+const selectShows = document.getElementById("selectShows");
+const amountResult = document.getElementById("amountResult");
 // Utils
 const formatNumbers = (num) => (num < 10 ? `0${num}` : num.toString());
 
@@ -21,7 +26,6 @@ const movieCataloguePublisher = MovieCataloguePublisher();
 const movieCatalogueData = {
   mc: [],
   set(mc) {
-    console.log(mc);
     this.movieCatalogue = mc;
     searchBar.value = "";
     selectMoviesElement.value = "";
@@ -32,6 +36,30 @@ const movieCatalogueData = {
   },
 };
 
+(function fetchDataShows() {
+  const url = "https://api.tvmaze.com/shows";
+  fetch(url).then((res) => {
+    res.json().then((data) => {
+      data
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .forEach(({ id, url, name }) => {
+          const option = document.createElement("option");
+          option.text = name;
+          option.value = id;
+          selectShows.add(option);
+        });
+    });
+  });
+})();
+
+selectShows.addEventListener("change", (e) => {
+  clearChildren(selectMoviesElement);
+  movieCataloguePublisher.subscribe(selectMovies);
+  e.target.value
+    ? fetchShow(`https://api.tvmaze.com/shows/${e.target.value}/episodes`)
+    : fetchShow();
+});
+
 function MovieCataloguePublisher() {
   let subscribers = [];
 
@@ -41,19 +69,23 @@ function MovieCataloguePublisher() {
   function unsubscribe(fn) {
     subscribers = subscribers.filter((sub) => sub !== fn);
   }
-  function inputChange(input) {
+  function inputChange(idInput, text) {
     let filteredEpisodes = movieCatalogueData.get();
-    if (input) {
+    if (text) {
       filteredEpisodes = movieCatalogueData
         .get()
         .filter(({ name, summary, season, number }) => {
           return (
             formatTitle({ name, season, number })
               .toLowerCase()
-              .includes(input.toLowerCase()) ||
-            summary.toLowerCase().includes(input.toLowerCase())
+              .includes(text.toLowerCase()) ||
+            summary.toLowerCase().includes(text.toLowerCase())
           );
         });
+    } else if (idInput) {
+      filteredEpisodes = movieCatalogueData
+        .get()
+        .filter(({ id }) => id.toString() === idInput);
     }
     subscribers.forEach((fn) => {
       fn(filteredEpisodes);
@@ -62,39 +94,34 @@ function MovieCataloguePublisher() {
   return { subscribe, inputChange, unsubscribe };
 }
 
-const movieCatalogue = document.getElementById("movieCatalogue");
-// Creating select movies
-const selectMoviesElement = document.getElementById("selectMovies");
-
 function selectMovies() {
-  movieCatalogueData.get().forEach(({ name, season, number }, i) => {
+  clearChildren(selectMoviesElement);
+  const optionShowAll = document.createElement("option");
+  optionShowAll.text = "Show all movies";
+  optionShowAll.value = "";
+  selectMoviesElement.add(optionShowAll);
+  movieCatalogueData.get().forEach(({ id, name, season, number }, i) => {
     const option = document.createElement("option");
     option.text = `${formatTitle({ name, season, number }, true)}`;
-    option.value = name;
+    option.value = id;
     selectMoviesElement.add(option);
   });
 }
+movieCataloguePublisher.subscribe(selectMovies);
 
-function clearSelect() {
-  selectMoviesElement.value = "";
-}
 selectMoviesElement.addEventListener("change", (e) => {
-  console.log(e.target.value);
+  movieCataloguePublisher.unsubscribe(selectMovies);
   movieCataloguePublisher.inputChange(e.target.value);
 });
 
 //Creating config search bar
-const searchBar = document.getElementById("searchBar");
 searchBar.addEventListener("input", (e) => {
-  clearSelect();
-  movieCataloguePublisher.inputChange(e.target.value);
+  movieCataloguePublisher.inputChange("", e.target.value);
 });
 
-const amountResult = document.getElementById("amountResult");
-
-const renderMovies = (arrOfObjects) => {
+const renderMovies = (arrOfMovies) => {
   clearChildren(movieCatalogue);
-  return arrOfObjects.map((e) => {
+  return arrOfMovies.map((e) => {
     const divContainer = document.createElement("div");
     const pName = document.createElement("p");
     const pSummary = document.createElement("p");
@@ -115,7 +142,15 @@ function renderAmountMovies(arrOfIndexToRender) {
   })`;
 }
 movieCataloguePublisher.subscribe(renderAmountMovies);
+
+const fetchShow = (url = "https://api.tvmaze.com/shows/82/episodes") => {
+  fetch(url).then((res) => {
+    res.json().then((data) => {
+      movieCatalogueData.set(data);
+    });
+  });
+};
+
 window.onload = function () {
-  movieCatalogueData.set(getAllEpisodes());
-  selectMovies();
+  fetchShow();
 };
